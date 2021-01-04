f493d05e36727a3bc77624542fc3ed3e95ced2bf
Andrew Eason <aeason952@gmail.com>
2021-01-04 13:40:37 -0500

    remove unused lines

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,16 +1,8 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
-import { AuthProvider } from "./context/AuthContext/AuthContext";
-import { UserProvider } from "./context/UserContext/UserContext";
 
 test("renders learn react link", () => {
-  const { container } = render(
-    <AuthProvider>
-      <UserProvider>
-        <App />
-      </UserProvider>
-    </AuthProvider>
-  );
+  const { container } = render(<App />);
   expect(container).toBeDefined();
 });
